6db59f0aac43e6382e1d0d7dd25166ac6e6b0c62
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2017-12-22 11:23:07 -0800

    Added a few more tip rack failure test cases

--- Tests/LHRP.Domain.Tests/Labware/Tips/TipRackShould.cs
+++ Tests/LHRP.Domain.Tests/Labware/Tips/TipRackShould.cs
@@ -35,5 +35,45 @@ namespace LHRP.Domain.Tests.Labware.Tips
             nextTipResult.Value.Row.Should().Be(1);
             nextTipResult.Value.Column.Should().Be(1);
         }
+
+        [Fact]
+        public void Refill()
+        {
+            var tipRack = new TipRack(8, 12, 300, true, 
+                new Position(), 9.0);
+            
+            tipRack.TotalTipCount.Should().Be(96);
+            tipRack.RemainingTips.Should().Be(96);
+
+            var result = tipRack.Consume(new LabwareAddress(1, 1));
+            result.IsSuccess.Should().BeTrue();
+            tipRack.RemainingTips.Should().Be(95);
+
+            tipRack.Refill();
+            tipRack.RemainingTips.Should().Be(96);
+        }
+
+        [Fact]
+        public void FailToConsumeAnInvalidLabwareAddress()
+        {
+            var tipRack = new TipRack(8, 12, 300, true, 
+                new Position(), 9.0);
+            var result = tipRack.Consume(new LabwareAddress(9999, 9999));
+            result.IsFailure.Should().BeTrue();
+        }
+
+        [Fact]
+        public void FailToGetTheNextTip_WhenTipRackIsEmpty()
+        {
+             var tipRack = new TipRack(1, 1, 300, true, 
+                new Position(), 9.0);
+
+            var result = tipRack.Consume(new LabwareAddress(1, 1));
+            result.IsSuccess.Should().BeTrue();
+            tipRack.RemainingTips.Should().Be(0);
+
+            var nextTipResult = tipRack.GetNextAvailableTip();
+            nextTipResult.IsFailure.Should().BeTrue();
+        }
     }
 }
