2c124ae82504b0a8ab884f3cfce0f7b728d24577
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2018-02-16 16:14:01 -0800

    Auto stash before merge of "transferPatterns" and "origin/transferPatterns"

--- LHRP.Api/Instrument/ITipManager.cs
+++ LHRP.Api/Instrument/ITipManager.cs
@@ -1,7 +1,12 @@
+using LHRP.Api.Common;
+using LHRP.Api.Devices.Pipettor;
+using LHRP.Api.Labware.Tips;
+
 namespace LHRP.Api.Instrument
 {
     public interface ITipManager
     {
-         
+         Result AssignTipRack(int positionId, TipRack tipRack);
+         Result RequestTips(ChannelPattern pattern);
     }
 }
