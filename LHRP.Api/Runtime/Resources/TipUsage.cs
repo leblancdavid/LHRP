26fbbd005004b20adb2528ae9c9c3b7ef56590ed
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2019-08-05 06:16:04 -0700

    Removed liquid and transfer history

--- LHRP.Api/Runtime/Resources/TipUsage.cs
+++ LHRP.Api/Runtime/Resources/TipUsage.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LHRP.Api.Instrument;
 
 namespace LHRP.Api.Runtime.Resources
 {
     public class TipUsage
     {
+        public int TipTypeId { get; private set; }
+        public int ExpectedTotalTipUsage { get; set; }
 
+        public TipUsage(int tipTypeId)
+        {
+            TipTypeId = tipTypeId;
+        }
+
+        public int GetExpectedReloadCount(IInstrument instrument)
+        {
+
+        }
     }
 }
