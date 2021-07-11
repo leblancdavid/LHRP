a0398c7a3a2ee65aa8ec4ed791a053be619baf7f
David Leblanc <lblancdavid@gmail.com>
2021-07-11 10:05:14 -0700

    Added a resource initializer

---
+++ LHRP.Api/Runtime/Resources/IResourceInitializer.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using LHRP.Api.Instrument;
+
+namespace LHRP.Api.Runtime.Resources
+{
+    public interface IResourceInitializer
+    {
+        Result Initialize(IInstrument instrument, ResourcesUsage resources);
+    }
+}
